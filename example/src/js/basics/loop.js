c17d483cac625d2275a2501703b0070d7fe2b729
thangngo <thangngo@asnet.com.vn>
2023-08-29 10:14:09 +0700

    Example with for

---
+++ example/src/js/basics/loop.js
@@ -0,0 +1,35 @@
+// For loop
+
+//  Tạo số ngẫu nhiên với loop
+function getRandNumbers (min, max, length) {
+  let arr = [];
+  for(let i = 1; i < length; i++) {
+    arr[i] = Math.random() * (max - min) + min
+  }
+  return arr
+}
+
+// console.log(getRandNumbers(2, 3, 10));
+
+// Tính tổng các số trong mảng
+function getTotal(arr) {
+  let sum = 0;
+  for(let i = 0; i < arr.length; i++) {
+    sum += arr[i]
+  }
+  return sum
+}
+
+// console.log(getTotal([1, 2, 3]));
+
+// lấy phần tử trong mảng
+let myArray = [
+  'Javascript',
+  'PHP',
+  'Java',
+  'React'
+]
+
+for (let i = 0; i < myArray.length; i++) {
+  console.log(myArray[i]);
+}
