9a14a2d1957b18a3a92dc323a1a95359636e53d7
thangngo <thangngo@asnet.com.vn>
2023-09-08 14:45:26 +0700

    Make exercise 2

---
+++ example/src/js/exercise/exercise_2/style.js
@@ -0,0 +1,8 @@
+function js_style() {
+  let text = document.getElementById('text');
+  text.style.fontSize = '14px'
+  text.style.color = 'blue'
+  text.style.fontFamily = 'Roboto, sans-serif'
+}
+
+export default js_style;
---
+++ example/src/js/exercise/index.js
@@ -0,0 +1,6 @@
+export { default } from './exercise_1/about.js';
+import book from './exercise_1/book.js'
+import js_style from './exercise_2/style.js';
+
+book()
+js_style()
