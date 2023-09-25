66df986562a9d5e7825ab0930c1b9f4ec0ba6fa1
thangngo <thangngo@asnet.com.vn>
2023-09-25 16:57:18 +0700

    Create regex file

--- src/js/constants/message.js
+++ src/js/constants/message.js
@@ -2,3 +2,13 @@ export const AUTHEN_MESSAGE = {
   loginError: 'Your Email or Password is incorrect, please try again',
   registerError: 'Your email already in use, please try again',
 };
+
+export const VALIDATE_MESSAGE = {
+  notValidEmail: 'Email is not valid',
+  requiredError: '{field} is required',
+  charactersPassWordError: 'Please enter at least 8 characters for password',
+  upperCasePassWordError: 'Please enter at least 1 uppercase letter',
+  lowercasePassWordError: 'Please enter at least 1 lowercase letter',
+  ANumberCharacterPassWordError: 'Please enter at least 1 number character',
+  confirmPasswordError: 'Password confirmation do not match password',
+};
