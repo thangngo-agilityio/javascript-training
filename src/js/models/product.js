990ec37124ff38915e82e3d01b3b560e3ad9941a
thangngo <thangngo@asnet.com.vn>
2023-10-05 11:31:10 +0700

    Fix naming and structure mvc

--- src/js/models/product.js
+++ src/js/models/product.js
@@ -1,3 +1,10 @@
+import {
+  PRODUCT_MESSAGE,
+  VALIDATE_MESSAGE
+} from "../constants/message";
+import {
+  Popup
+} from "../helpers/renderPopup";
 import HttpsService from "../service/httpsService";
 
 /**
@@ -5,6 +12,7 @@ import HttpsService from "../service/httpsService";
  * Manages the products data
  */
 export default class ProductModel {
+  popup = new Popup()
   constructor() {
     this.productService = new HttpsService('products')
   }
@@ -14,7 +22,13 @@ export default class ProductModel {
    * @return list product return after make a GET request to server
    */
   async getProduct(query) {
-    return await this.productService.get(query)
+    try {
+      return await this.productService.get(query)
+    } catch (error) {
+      this.popup.error({
+        message: VALIDATE_MESSAGE.getFailed,
+      });
+    }
   }
 
   /**
@@ -22,29 +36,63 @@ export default class ProductModel {
    * @return list product return after make a GET request to server
    */
 
-  async getProductId(id) {
-    return await this.productService.getById(id)
+  async getProductById(id) {
+    try {
+      return await this.productService.getById(id)
+    } catch {
+      this.popup.error({
+        message: VALIDATE_MESSAGE.getFailed
+      });
+    }
   }
 
   /**
    * @description create new product item and save response to mock api return product item
    */
   handleAddProduct = async (data) => {
-    const newProduct = {
-      name: data.name,
-      price: Number(data.price),
-      image: data.image,
-      quantity: Number(data.quantity),
-    };
+    try {
+      const newProduct = {
+        name: data.name,
+        price: Number(data.price),
+        image: data.image,
+        quantity: Number(data.quantity),
+      };
 
-    return await this.productService.post(newProduct)
+      this.popup.success({
+        message: PRODUCT_MESSAGE.addSuccess,
+      });
+
+      return await this.productService.post(newProduct)
+    } catch {
+      this.popup.error({
+        message: PRODUCT_MESSAGE.addFailed,
+      });
+    }
   }
 
   handleDelProduct = async (id) => {
-    return await this.productService.delete(id);
+    try {
+      await this.productService.delete(id);
+      this.popup.success({
+        message: PRODUCT_MESSAGE.removeSuccess,
+      });
+    } catch {
+      this.popup.error({
+        message: PRODUCT_MESSAGE.removeFail,
+      });
+    }
   }
 
   handleEditProduct = async (data, id) => {
-    return await this.productService.put(data, id)
+    try {
+      await this.productService.put(data, id);
+      this.popup.success({
+        message: PRODUCT_MESSAGE.editSuccess,
+      });
+    } catch {
+      this.popup.error({
+        message: PRODUCT_MESSAGE.editFail,
+      });
+    }
   }
 }
