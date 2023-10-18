6e3fad0fcc50625288ba07fc7de41404f92d704f
thangngo <thangngo@asnet.com.vn>
2023-10-18 17:00:43 +0700

    Add folder static

---
+++ src/js/utils/debounce.js
@@ -0,0 +1,14 @@
+/**
+ * @description technique of forcing a function to wait a certain amount of time before executing
+ */
+export const debounce = (func, delay) => {
+  let timeoutId;
+
+  return function (...args) {
+    clearTimeout(timeoutId);
+
+    timeoutId = setTimeout(() => {
+      func.apply(this, args);
+    }, delay);
+  };
+};
